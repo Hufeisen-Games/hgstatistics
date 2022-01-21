6bdc3d97f5da5bef0d5f2c77a04369487c33ec91
Hufeisen <hufeisen@hufeisen-games.de>
2022-01-21 20:30:24 +0100

    Added download link to update scheduler

--- src/main/java/de/hufeisen_games/repo/hgstatistics/listener/PlayerJoinListener.java
+++ src/main/java/de/hufeisen_games/repo/hgstatistics/listener/PlayerJoinListener.java
@@ -11,6 +11,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import main.java.de.hufeisen_games.repo.hgstatistics.HGStatistics;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ClickEvent.Action;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class PlayerJoinListener implements Listener {
 
@@ -29,7 +32,16 @@ public class PlayerJoinListener implements Listener {
 					e.getPlayer().sendMessage("§7--------- §6§lHGStatistics §6§oV. "
 							+ HGStatistics.getPlugin().getDescription().getVersion() + " §6(Update) §7---------");
 					e.getPlayer().sendMessage("");
-					e.getPlayer().sendMessage("§a§lThere is an update available (version §6§l"+version+"§l§a)");
+					
+					TextComponent updateMessage = new TextComponent("§a§lThere is an update available (version §6§l"+version+"§a§l). Press ");
+					TextComponent updateLink = new TextComponent("§c§nhere");
+					
+					updateLink.setClickEvent(new ClickEvent(Action.OPEN_URL, "https://codeinfo.hufeisen-games.de/hgstatistics/download/latest"));
+					
+					updateMessage.addExtra(updateLink);
+					updateMessage.addExtra(" §a§lto download!");
+					
+					e.getPlayer().spigot().sendMessage(updateMessage);
 					e.getPlayer().sendMessage("");
 					e.getPlayer().sendMessage("§7--------- §6§lHGStatistics §6§oV. "
 							+ HGStatistics.getPlugin().getDescription().getVersion() + " §6(Update) §7---------");
