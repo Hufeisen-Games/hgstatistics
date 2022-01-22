a9d9e8dabb94df139ee1828e8738a206eb739bbd
Hufeisen <hufeisen@hufeisen-games.de>
2022-01-22 13:11:56 +0100

    Fixed Craft Items statistic

--- src/main/java/de/hufeisen_games/repo/hgstatistics/listener/BufferUpdateListener.java
+++ src/main/java/de/hufeisen_games/repo/hgstatistics/listener/BufferUpdateListener.java
@@ -1,22 +1,25 @@
 package main.java.de.hufeisen_games.repo.hgstatistics.listener;
 
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.Statistic;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.player.PlayerItemBreakEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import main.java.de.hufeisen_games.repo.hgstatistics.HGStatistics;
 import main.java.de.hufeisen_games.repo.hgstatistics.StatisticBuffer;
 
 public class BufferUpdateListener implements Listener {
 
+	public int currentAmount = 0;
+	
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e) {
 		if (!StatisticBuffer.isLoading) {
@@ -30,27 +33,43 @@ public class BufferUpdateListener implements Listener {
 	@EventHandler
 	public void onItemCrafted(CraftItemEvent e) {
 		if (!StatisticBuffer.isLoading) {
-			ItemStack craftedItem = e.getInventory().getResult();
-			Inventory Inventory = e.getInventory();
-			ClickType clickType = e.getClick();
-
-			int realAmount = craftedItem.getAmount();
-
-			if (clickType.isShiftClick()) {
-
-				int lowerAmount = craftedItem.getMaxStackSize() + 1000;
-
-				for (ItemStack actualItem : Inventory.getContents()) {
-
-					if (!actualItem.getType().isAir() && lowerAmount > actualItem.getAmount()
-							&& !actualItem.getType().equals(craftedItem.getType()))
-						lowerAmount = actualItem.getAmount();
+			Material craftedItemType = e.getCurrentItem().getType();
+			
+			for(ItemStack s : e.getInventory().getContents()) {
+				if(s.getType() == craftedItemType) {
+					
+					currentAmount += s.getAmount();
+					
 				}
-
-				realAmount = lowerAmount * craftedItem.getAmount();
 			}
+			
+			System.out.println(currentAmount);
+			
+			new BukkitRunnable() {
+				
+				@Override
+				public void run() {
+					
+					int newAmount = 0;
+					
+					for(ItemStack s : e.getWhoClicked().getInventory().getContents()) {
+						if(s != null) {
+							System.out.println(s.getType());
+							if(s.getType() == craftedItemType) {
+								
+								newAmount += s.getAmount();
+								
+							}
+						}
+					}
+					System.out.println(craftedItemType);
+					System.out.println();
+					System.out.println(newAmount);
+					StatisticBuffer.addToCache(Statistic.CRAFT_ITEM, e.getWhoClicked().getName(), newAmount-currentAmount);
+					
+				}
+			}.runTaskLaterAsynchronously(HGStatistics.getPlugin(), 20);
 
-			StatisticBuffer.addToCache(Statistic.CRAFT_ITEM, e.getWhoClicked().getName(), realAmount);
 		}
 	}
 
